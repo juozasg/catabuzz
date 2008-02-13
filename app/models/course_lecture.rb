0a4888ab54c80bf4fe989730ff99c13b0ab571ee
Juozas Gaigalas <juozas@tuesdaystudios.com>
2008-02-13 05:32:11 +0000

    more ferreted fields

--- app/models/course_lecture.rb
+++ app/models/course_lecture.rb
@@ -1,18 +1,22 @@
 class CourseLecture < ActiveRecord::Base
+  acts_as_ferret :field => [:days, :start_time, :end_time, :instructor]
 	belongs_to :course
 	has_and_belongs_to_many :schedules
 	
-	# courseLectureCode:string
-	# enrollmentMax:int
-	# enrollmentCurrent:int
-	# days:string
-	# startTime:int
-	# endTime:int
-	# locationCode:string
-	# instructorName:string
-	# lectureType:string
+  # t.string  "course_lecture_code"
+  # t.integer "enrollment_max"
+  # t.integer "enrollment_current"
+  # t.string  "days"
+  # t.integer "start_time"
+  # t.integer "end_time"
+  # t.string  "location"
+  # t.string  "instructor"
+  # t.string  "update_url"  (example: "http://info.sjsu.edu/web-dbgen/soc-fall-courses/c667552.html")
+  # t.string  "footnotes"
+  # t.string  "type"
+  # t.string  "section"
+  # t.integer "course_id",           :null => false
 	
-	# updateURL:string (example: "http://info.sjsu.edu/web-dbgen/soc-fall-courses/c667552.html")
 	
 	validates_presence_of :course
 	validates_presence_of :schedules
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,5 @@
 class Schedule < ActiveRecord::Base
+  acts_as_ferret :field => [:name]
 	has_and_belongs_to_many :course_lectures
 	
 	# name:string
