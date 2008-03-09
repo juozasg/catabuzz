510688be57cf2c396fed12087d1df3554192f31d
Juozas Gaigalas <juozas@tuesdaystudios.com>
2008-03-09 08:58:27 +0000

    serious style

--- app/helpers/course_finder_helper.rb
+++ app/helpers/course_finder_helper.rb
@@ -5,4 +5,22 @@ module CourseFinderHelper
     sprintf("%.2d:%.2d", hours, mins)
   end
   
+  def format_enrollment(current, max)
+    return "#{current} / #{max}"
+  end
+  
+  def get_enrollment_cell_class(current, max)
+      return "enrollment_unavailable" if max.to_i < 1
+      case current / max.to_f 
+      when 0.0...0.8
+        "enrollment_available"
+      when 0.8...1.0
+        "enrollment_near_full"
+      when 1.0...10.0
+        "enrollment_full"
+      else
+        raise "Strange enrollment numbers here!: #{current / max.to_f}"
+      end
+  end
+  
 end
