78bc64bc3171a5629033d9b7766d263a9c1c1e7a
Juozas Gaigalas <juozas@tuesdaystudios.com>
2008-03-12 23:29:59 +0000

    building query test

--- test/unit/query_parser_test.rb
+++ test/unit/query_parser_test.rb
@@ -1,8 +1,10 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require "test/unit"
+require "../../lib/search_query_parser.rb"
 
-class QueryParserTest < ActiveSupport::TestCase
+class QueryParserTest < Test::Unit::TestCase
   # Replace this with your real tests.
   def test_truth
+  	s = SearchQueryParser.build_ferret_query("lala")
     assert true
   end
 end
