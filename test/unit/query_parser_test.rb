508c17ef97a3cf1787bf75543a17395a39b1d195
juozas.gaigalas <juozas.gaigalas@XP25-JGAIGALAS.thqinc.com>
2008-04-15 16:47:26 -0700

    Added course code parsing

--- test/unit/query_parser_test.rb
+++ test/unit/query_parser_test.rb
@@ -7,12 +7,14 @@ class QueryParserTest < Test::Unit::TestCase
   end
   # Replace this with your real tests.
   def test_run_method
+    return
     s = SearchQueryParser.build_ferret_query("T")
     s = SearchQueryParser.build_ferret_query("")
     assert true
   end
 
   def test_daysofweek
+    return
     days = ["M", "T", "W", "R", "F", "S"]
     # test upper and lower case
     for d in days do
@@ -27,12 +29,23 @@ class QueryParserTest < Test::Unit::TestCase
       assert_equal "days:#{c.upcase}", SearchQueryParser.build_ferret_query("#{c}")
     end
 
+    # test TBAs
+    tbas = ["TBA", "tba", "Tba", "tBa", "tbA", "TBa", "tBA", "TbA"]
+    for t in tbas do
+      assert_equal "days:#{t.upcase}", SearchQueryParser.build_ferret_query("#{t}")
+    end
+
   end
 
+  def test_codes
+    codes = ["LING101", "A1", "A1b", "bob999d"]
+    for c in codes do
+      assert_equal "code:#{c.upcase}", SearchQueryParser.build_ferret_query("#{c}")
+    end
+  end
+  
+
   def test_combos
-    q = SearchQueryParser.build_ferret_query("TW")
-    assert_equal "days:TW", q
-    
   	q = SearchQueryParser.build_ferret_query("ADV101")
     assert_equal "code:ADV101", q
     
@@ -41,7 +54,6 @@ class QueryParserTest < Test::Unit::TestCase
     
     q = SearchQueryParser.build_ferret_query("ADV101 t")
     assert_equal "code:ADV101 days:T", q
-    
-
   end
+
 end
