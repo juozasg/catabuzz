d8842fd02babf29526a6c51b4b4abfb8c56709de
Juozas Gaigalas <juozas@hobo23.(none)>
2008-05-28 00:31:16 -0700

    removed course section: destroy

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -13,17 +13,4 @@ class CourseSectionsController < ApplicationController
   end
 
 
-
-  # DELETE /course_sections/1
-  # DELETE /course_sections/1.xml
-  def destroy
-    # @course_section = CourseSection.find(params[:id])
-    #    @course_section.destroy
-    # 
-    #    respond_to do |format|
-    #      format.html { redirect_to(course_sections_url) }
-    #      format.xml  { head :ok }
-    #    end
-  end
-
 end
