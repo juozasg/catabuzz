243e3d846a5dce582c8bc5b1711bb87b29128e0a
Juozas Gaigalas <juozas@hobo23.(none)>
2008-06-01 02:56:30 -0700

    updated course full css. messing round with ajaxy course sections

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -1,18 +1,7 @@
 class CourseSectionsController < ApplicationController
   # GET /course_sections/1
-  # GET /course_sections/1.xml
   def show
     @course_section = CourseSection.find(params[:id])
     render :layout => false
-    #render :text => "got it #{params[:id]}"
-  
-#    @course_section = CourseSection.find(params[:id])
-
-#    respond_to do |format|
-#      format.html # show.html.erb
-#      format.xml  { render :xml => @course_section }
-#    end
   end
-
-
 end
