93f8ae3bab66b91cf1f7d43a41282b6c73df99db
Juozas Gaigalas <juozas@hobo23.(none)>
2008-06-10 00:36:20 -0700

    added notes lookup

--- app/helpers/course_sections_helper.rb
+++ app/helpers/course_sections_helper.rb
@@ -1,2 +1,13 @@
+require 'csv'
+
 module CourseSectionsHelper
+  def createNotesTable
+    @notesTable = []
+    CSV.open("public/classnotes.csv", "r").each { |row| @notesTable << row[1]}
+  end
+  
+  def getNoteText(noteNumber)
+    createNotesTable if(@notesTable.nil?)
+    return @notesTable[noteNumer - 1]
+  end
 end
