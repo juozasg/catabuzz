001b8398171c707e6000b0d96d625c42748163d6
Juozas Gaigalas <juozas@hobo23.(none)>
2008-06-12 01:11:57 -0700

    Loading text is working

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -2,6 +2,7 @@ class CourseSectionsController < ApplicationController
   
   # GET /course_sections/1
   def show
+    sleep 3
     @section = CourseSection.find(params[:id])
     layout = "course_section_show_external"
     layout = false if params[:ajax]
--- public/javascripts/course_finder.js
+++ public/javascripts/course_finder.js
@@ -1,18 +1,66 @@
+loadingText = "";
+
+function updateLoadingText(e)
+{
+  var str = $(e).text();
+  var dots = str.slice(str.indexOf("."), str.lastIndexOf(".") + 1);
+  var numDots = dots.length;
+  
+  if(numDots >= 3)
+  {
+    numDots = 0;
+  }
+  else
+  {
+    numDots ++;
+  }
+  
+  str = "";
+  while(numDots > 0)
+  {
+    str += ".";
+    numDots--;
+  }
+  
+  str = "Loading" + str;
+  $(e).text(str);
+}
+
 $(document).ready(function()
 {
+  
+
   var createLoadDropdown = function(row, href)
   {
     var cols = row.children().size();
     var tr = $("<tr></tr>").addClass("dropdown");    
     var td = $("<td></td").attr("colspan", cols);
-    var container = $("<div class='dropdown_container'></div>").appendTo(td);
     
-    container.append("lalal");
     tr.append(td);
-    container.hide();
     tr.insertAfter($(row));
-    container.load(href + "?ajax");
-    container.slideDown('fast');
+    
+	  var loadingContainer = $("<div class='loading_container'></div>").appendTo(td);
+    var container = $("<div class='dropdown_container'></div>").appendTo(td);
+  
+    container.hide();
+	  loadingContainer.hide();
+	    
+    loadingContainer.append("<h4>Loading.</h4>");
+    loadingContainer.fadeIn('fast');
+
+    // flash ... in loading container
+    loadingText = $(loadingContainer).find("h4")
+    var interval = setInterval('updateLoadingText(loadingText);', 500);
+
+    container.load(href + "?ajax", function()
+	  {
+	    clearInterval(interval);
+	    loadingContainer.hide();
+	    loadingContainer.remove();
+	    container.slideDown('fast');
+	  }
+	  );
+    
   }
   
   var destroyDropdown = function(row)
