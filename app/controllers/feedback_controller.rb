63e193b7701c7e5162647d605f4bed2adf894f64
Juozas Gaigalas <juozas@hobo23.(none)>
2008-07-02 16:50:29 -0700

    added the feedback form page

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,6 +1,10 @@
 class FeedbackController < ApplicationController
   def index
-    render :text => "please enter your feedback"
+    # render :text => "please enter your feedback"
+  end
+  
+  def create
+    render :text => "form submitted"
   end
   
 end
