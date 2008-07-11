cf412fd3083ce45619db08c590d5b9f6b9ec79e1
Juozas Gaigalas <juozas@hobo23.(none)>
2008-07-11 03:35:10 -0700

    "Sending feedback" message

--- public/javascripts/feedback_form.js
+++ public/javascripts/feedback_form.js
@@ -3,9 +3,10 @@ $(document).ready(function()
   
   $("input#send_feedback").click(function()
   {
+    $("#feedback_notification").text("(Sending feedback...)").fadeOut('slow').fadeIn('slow');
     $("form.feedback").ajaxSubmit(function(){
       $("#feedback_notification").text("(Thanks!)").css("font-weight", "bold")
-        .fadeOut().fadeIn().fadeOut().fadeIn().fadeOut();
+        .fadeOut().fadeIn().fadeOut().fadeIn().fadeOut().fadeIn();
     });
     return false;
   });
