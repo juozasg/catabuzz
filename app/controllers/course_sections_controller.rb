868c4a4499ce99d2652c20a30f8239ebd88c5639
Juozas Gaigalas <juozas@hobo23.(none)>
2008-07-10 23:58:16 -0700

    Removed accidentally commited debug code

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -2,8 +2,6 @@ class CourseSectionsController < ApplicationController
   
   # GET /course_sections/1
   def show
-    
-    sleep 3
     @section = CourseSection.find(params[:id])
     layout = "course_section_show_external"
     layout = false if params.include? :ajax
