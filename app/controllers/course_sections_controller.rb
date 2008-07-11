add3c6ec1a75cef04df57df7b3fcd179e853bdb9
Juozas Gaigalas <juozas@hobo23.(none)>
2008-07-10 23:48:46 -0700

    Fixed course section GET ajax bug (ajax requests were rendered with layout)

--- app/controllers/course_sections_controller.rb
+++ app/controllers/course_sections_controller.rb
@@ -2,9 +2,11 @@ class CourseSectionsController < ApplicationController
   
   # GET /course_sections/1
   def show
+    
+    sleep 3
     @section = CourseSection.find(params[:id])
     layout = "course_section_show_external"
-    layout = false if params[:ajax]
+    layout = false if params.include? :ajax
     render :layout => layout
   end
 end
