0842affc5952c8432cbf9d83c574d60680a3683a
Juozas Gaigalas <juozas@hobo23.(none)>
2008-07-11 18:17:06 -0700

    Sending feedback message blinks one extra time.

--- public/javascripts/feedback_form.js
+++ public/javascripts/feedback_form.js
@@ -3,7 +3,7 @@ $(document).ready(function()
   
   $("input#send_feedback").click(function()
   {
-    $("#feedback_notification").text("(Sending feedback...)").fadeOut('slow').fadeIn('slow');
+    $("#feedback_notification").text("(Sending feedback...)").fadeOut('slow').fadeIn('slow').fadeOut('slow').fadeIn('slow');
     $("form.feedback").ajaxSubmit(function(){
       $("#feedback_notification").text("(Thanks!)").css("font-weight", "bold")
         .fadeOut().fadeIn().fadeOut().fadeIn().fadeOut().fadeIn();
